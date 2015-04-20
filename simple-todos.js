6114fd603ea470c39412a04c17e7f1fe33fabcfe
Tianlong Wang <tianlong.wang.13@googlemail.com>
2015-04-20 17:36:23 +0800

    input form, part 3 things

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,32 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.body.helpers({
+        tasks: function() {
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
+    Template.body.events({
+        "submit .new-task": function(event) {
+            // This function is called when the new task form is submitted
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+            var text = event.target.text.value;
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        }
+    });
 }
-
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
